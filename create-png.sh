002ae77336d3020089b2397117a5bf3bfd76b9fd
Matthias Wientapper <m.wientapper@gmx.de>
2020-03-14 13:20:41 +0100

    Display error png in case data cannot be read

--- create-png.sh
+++ create-png.sh
@@ -4,5 +4,10 @@ PWD=$(pwd)
 export LD_LIBRARY_PATH=$PWD/lib
 
 $PWD/weather2svg.py
-$PWD/bin/rsvg-convert --background-color=white -o out.png weather-script-output.svg
-$PWD/bin/pngcrush -q -c 0 out.png kindle-weather.png
+if [ $? -eq 0 ]
+then
+    $PWD/bin/rsvg-convert --background-color=white -o out.png weather-script-output.svg
+    $PWD/bin/pngcrush -q -c 0 out.png kindle-weather.png
+else
+    cp error.png kindle-weather.png
+fi
