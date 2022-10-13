e6d1be43fa4a5b6400b4cb88428ff480a2b60137
Josef Larsson <josef.lar@gmail.com>
2022-10-13 21:46:20 +0900

    Fix random hangs on Kindle PW (EY21)

--- kindle-weather.sh
+++ kindle-weather.sh
@@ -137,7 +137,8 @@ while true; do
     ### flight mode on...
     lipc-set-prop com.lab126.cmd wirelessEnable 0
 
-	sleep 2
+    ### 3 secs necessary on Kindle PW (EY21) to avoid random hangs
+	sleep 3
 
     ### set wake up time to one hour
 	rtcwake -d /dev/rtc1 -m no -s $SLEEP_SECONDS
